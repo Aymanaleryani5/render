2631850d5cbbb6a2e9f72b11e3571b19a3377a36
Aymanaleryani5 <167556283+Aymanaleryani5@users.noreply.github.com>
2026-07-31 21:41:45 +0300

    server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ const rateLimiter = rateLimit({
 // ==========================================================
 const SUPABASE_URL = process.env.SUPABASE_URL || "https://qfcsaiyuyxhibidrrmha.supabase.co";
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || "";
-const FIRECRAWL_API_KEY = "fc-72e13b51534b49c2a7294dc16ccf479d";
+const FIRECRAWL_API_KEY = "fc-c06dc781b8a546168460a30ba114c359";
 
 // إنشاء مثيلات
 const cache = new MemoryCache();
