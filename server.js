a30e5728c22557ce3a54e1f3f6a655dd2f8c6748
Aymanaleryani5 <167556283+Aymanaleryani5@users.noreply.github.com>
2026-08-07 03:20:11 +0300

    server.js

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ const rateLimiter = rateLimit({
 // ==========================================================
 const SUPABASE_URL = process.env.SUPABASE_URL || "https://qfcsaiyuyxhibidrrmha.supabase.co";
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || "";
-const SCRAPINGBEE_API_KEY = process.env.SCRAPINGBEE_API_KEY || "5CATOEES95GL3KA4XVMRY1CABK3N5TUXT18N0M1R1P3XW4CY1XM4J1X6WZ2K4DFUE059R6IHUBNRWCT7";
+const SCRAPINGBEE_API_KEY = process.env.SCRAPINGBEE_API_KEY || "39N39PJ5AA62SEB2F44E0GIFZPQ58PSG1AZXI1N3MUBJ8J8CWOYV1R8LNFJMRTMWYVAT5ER7HC80X4N4";
 
 // إنشاء مثيلات
 const cache = new MemoryCache();
